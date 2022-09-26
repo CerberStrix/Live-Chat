520b3f6573380e1bcc66eaf873a681137684555e
Alisher Kim <cerberstrix@gmail.com>
2022-09-27 02:53:19 +0500

    fix pkg lock

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,29 +1,3 @@
-import '../App.css';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-
-import Home from './Home';
-import Login from './Login';
-import NotFound from './NotFound';
-import NavBar from './NavBar';
-
-const App = () => (
-  <div className="App">
-
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  </div>
-);
+const App = () => <h1>Hexlet Chat</h1>;
 
 export default App;
--- frontend/src/utils/validationSchema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const loginSchema = yup.object().shape({
-  name: yup.string().min(3).max(15).required(),
-  password: yup.string().required(),
-});
-
-export default loginSchema;
