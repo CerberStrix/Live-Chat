5d26170132a572b1435f86171d735c2ff3a2578e
Alisher Kim <cerberstrix@gmail.com>
2022-10-03 12:05:44 +0500

    clean install

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './components/App.jsx';
-import './i18n';
-import store from './slices/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-);
+root.render(<App />);
