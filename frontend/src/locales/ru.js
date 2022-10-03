e7ac17dcab0052151fb103a8a8727c22bb89b21c
Alisher Kim <cerberstrix@gmail.com>
2022-10-03 11:57:23 +0500

    clean install

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,15 +1,1 @@
-export default {
-  translation: {
-    join: 'Войти',
-    'your nickname': 'Ваш никнейм',
-    password: 'Пароль',
-    registration: 'Регистрация',
-    'doesnt hava account?': 'Нет аккаунта?',
-    incorrectAuthData: 'Неправильный логин или пароль',
-    'log out': 'Выйти',
-    channels: 'Каналы',
-    'enter messages': 'Введите сообщение',
-    message_count: 'сообщений',
-    enter_message: 'Введите сообщение...',
-  },
-};
+export default {};
